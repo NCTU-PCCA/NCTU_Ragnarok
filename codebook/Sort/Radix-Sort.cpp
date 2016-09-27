cce49a0fadaa4bc52ac32328696239764c15b6a0
0Alien0 <jeffrey6910@gmail.com>
2016-09-27 11:22:49 +0800

    Update Radix-Sort.cpp

--- codebook/Sort/Radix-Sort.cpp
+++ codebook/Sort/Radix-Sort.cpp
@@ -1,7 +1,7 @@
-int maxbit(int data[], int n) //辅助函数，求数据的最大位数
+int maxbit(int data[], int n) //輔助函数，求數据的最大位數
 {
-    int maxData = data[0];      ///< 最大数
-    /// 先求出最大数，再求其位数，这样有原先依次每个数判断其位数，稍微优化点。
+    int maxData = data[0];      ///< 最大數
+    /// 先求出最大數，再求其位數，这样有原先依次每个數判斷其位數，稍微優化點。
     for (int i = 1; i < n; ++i) {
         if (maxData < data[i]) maxData = data[i];
     }
@@ -11,7 +11,7 @@ int maxbit(int data[], int n) //辅助函数，求数据的最大位数
         ++d;
     }
     return d;
-    /*    int d = 1; //保存最大的位数
+    /*    int d = 1; //保存最大的位數
           int p = 10;
           for(int i = 0; i < n; ++i){
           while(data[i] >= p){
@@ -21,26 +21,26 @@ int maxbit(int data[], int n) //辅助函数，求数据的最大位数
           }
           return d;*/
 }
-void radixsort(int data[], int n) //基数排序
+void radixsort(int data[], int n) //基數排序
 {
     int d = maxbit(data, n);
     int *tmp = new int[n];
-    int *count = new int[10]; //计数器
+    int *count = new int[10]; //計數器
     int i, j, k;
     int radix = 1;
-    for(i = 1; i <= d; i++) { //进行d次排序
-        for(j = 0; j < 10; j++) count[j] = 0; //每次分配前清空计数器
+    for(i = 1; i <= d; i++) { //進行d次排序
+        for(j = 0; j < 10; j++) count[j] = 0; //每次分配前清空计數器
         for(j = 0; j < n; j++){
-            k = (data[j] / radix) % 10; //统计每个桶中的记录数
+            k = (data[j] / radix) % 10; //統计每個桶中的記錄數
             count[k]++;
         }
-        for(j = 1; j < 10; j++) count[j] = count[j - 1] + count[j]; //将tmp中的位置依次分配给每个桶
-        for(j = n - 1; j >= 0; j--) { //将所有桶中记录依次收集到tmp中
+        for(j = 1; j < 10; j++) count[j] = count[j - 1] + count[j]; //將tmp中的位置依次分配给每個桶
+        for(j = n - 1; j >= 0; j--) { //將所有桶中記錄依次收集到tmp中
             k = (data[j] / radix) % 10;
             tmp[count[k] - 1] = data[j];
             count[k]--;
         }
-        for(j = 0; j < n; j++) //将临时数组的内容复制到data中
+        for(j = 0; j < n; j++) //將臨時數组的内容複製到data中
             data[j] = tmp[j];
         radix = radix * 10;
     }
