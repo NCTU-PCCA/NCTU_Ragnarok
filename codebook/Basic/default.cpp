369e13e70aa312a476de53a57a448259e5b497ab
0Alien0 <jeffrey6910@gmail.com>
2016-09-27 10:57:21 +0800

    Create default.cpp

---
+++ codebook/Basic/default.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define FI freopen("in.txt", "r", stdin)
+#define FO freopen("out.txt", "w", stdout)
+#define IOS ios_base::sync_with_stdio(0);cin.tie(0)
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+
+typedef long long LL;
+
+const int MOD = 1000000007;
+const double PI = acos(-1.0);
+
+int dx[] = {-1,0,1,0};
+int dy[] = {0,1,0,-1};
+
+int main(){
+    IOS;
+    return 0;
+}
