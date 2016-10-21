e6c7c496f2a92dcd2e8ec3597d6be6ffbfd2be26
傅裕夫 <a0919610611@gmail.com>
2016-10-22 00:08:50 +0800

    add manhattan_mst and hasn't test

---
+++ codebook/Graph/manhattan_mst.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+const int N = 10000;
+struct Point {
+    int x, y, id;
+    bool operator<(const Point& p) const
+    {
+        return x != p.x ? x < p.x : y < p.y;
+    }
+};
+
+struct BIT {
+    int min_val, pos;
+    void init()
+    {
+        min_val = INT_MAX;
+        pos = -1;
+    }
+} bit[N];
+struct Edge {
+    int u, v, d;
+    Edge() {}
+    Edge(int _u, int _v, int _d)
+        : u(_u)
+        , v(_v)
+        , d(_d)
+    {
+    }
+    bool operator<(const Edge& e) const
+    {
+        return d < e.d;
+    }
+};
+vector<Point> p;
+vector<Edge> E;
+int T[N], hs[N];
+int m, mt;
+int ds[N]; //disjoint set for kruskal
+
+void add_edge(int u, int v, int d)
+{
+    E.push_back(Edge(u, v, d));
+}
+int find(int x)
+{
+    return ds[x]=(x==ds[x]?x:find(ds[x]);
+}
+int dist(int i, int j)
+{
+    return abs(p[i].x - p[j].x) + abs(p[i].y - p[j].y);
+}
+inline int lowbit(int x)
+{
+    return x & (-x);
+}
+void update(int x, int val, int pos)
+{
+    for (int i = x; i >= 1; i -= lowbit(i)) {
+        if (val < bit[i].min_val)
+            bit[i].min_val = val, bit[i].pos = pos;
+    }
+}
+int query(int x, int m)
+{
+    int min_val = INT_MAX, pos = -1;
+    for (int i = x; i <= m; i += lowbit[i]) {
+        if (bit[i].min_val < min_val) {
+            min_val = bit[i].min_val, pos = bit[i].pos;
+        }
+    }
+    return pos
+}
+int Manhattan_MST(vector<Point>& P, int k)
+{
+    mt = 0;
+    int n=P.size();
+    for (int dir = 0; dir < 4; dir++) {
+        if (dir == 1 || dir == 3) {
+            for (int i = 0; i < n; i++)
+                swap(P[i].x, P[i].y);
+        } else if (dir == 2) {
+            for (int i = 0; i < n; i++)
+                P[i].x = -P[i].x;
+        }
+    }
+    sort(P.begin(),P.end());
+    for(int i=0;i<n;i++){ //discretize 
+        T[i]=hs[i]=p[i].y-p[i].x;
+    }
+    sort(hs,hs+n;)
+    m=unique(hs,hs+n)-hs;
+    for(int i=1;i<=m;i++) bit[i].init();
+    for(int i=n-1;i>=0;i--){
+        pos=lower_bound(hs,hs+m,T[i])-hs+1; //Bit is 1-based 
+        w=query(pos,m);
+        if(w!=-1) add_edge(p[i].id,p[w].id,dist(i,w));
+        update(pos,p[i].x+p[i].y,i);
+    }
+    sort(E.begin(),E.end());
+    int ans=0;
+    int p=1;
+    for(int i=0;i<n;i++) ds[i]=i;
+    for(int i=0;i<E.size() && p<=n;i++){
+       int  fa=find(E[i].u);
+       int fb=find(E[i].v);
+       if(fa!=fb){
+          p++;ds[fa]=fb;
+          ans+=E[i].d;
+       }
+    }
+    return ans;
+
+}
