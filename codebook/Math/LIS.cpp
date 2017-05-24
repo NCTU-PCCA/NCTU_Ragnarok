94883c535f16f29c2ec25ea8a957852491f621ec
a0919610611 <a0919610611@gmail.com>
2017-05-24 23:09:51 +0800

    add ex LIS

--- codebook/Math/LIS.cpp
+++ codebook/Math/LIS.cpp
@@ -1,6 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
+template <typename E>
+struct Node {
+    E value;
+    E* pointer;
+};
 
+template <class E>
+struct node_ptr_less {
+    bool operator()(E*& node1,
+        E*& node2) const
+    {
+        return node1->value < node2->value;
+    }
+};
+template <typename E>
+std::vector<E> lis(const std::vector<E>& n)
+{
+    typedef E* NodePtr;
+
+    std::vector<NodePtr> pileTops;
+    // sort into piles
+    for (typename std::vector<E>::const_iterator it = n.begin(); it != n.end(); it++) {
+        NodePtr node(new Node<E>());
+        node->value = *it;
+        typename std::vector<NodePtr>::iterator j = std::lower_bound(pileTops.begin(), pileTops.end(), node, node_ptr_less<E>());
+        if (j != pileTops.begin())
+            node->pointer = *(j - 1);
+        if (j != pileTops.end())
+            *j = node;
+        else
+            pileTops.push_back(node);
+    }
+    // extract LIS from piles
+    std::vector<E> result;
+    for (NodePtr node = pileTops.back(); node != nullptr; node = node->pointer)
+        result.push_back(node->value);
+    std::reverse(result.begin(), result.end());
+    return result;
+}
 int LIS(vector<int>& v)
 {
     vector<int> ans;
